d65d3e73e36e55abc6e0150872a6c5440a3a9d16
Thiyagarajan <thiyagarajan168@gmail.com>
2021-09-30 19:06:39 +0530

    basic dotnet core practice

---
+++ ExploreCalifornia/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FeatureToggle>(x=> new FeatureToggle{
+                DeveloperExceptions = Configuration.GetValue<bool>("FeatureToggle:DeveloperExceptions")
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggle features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DeveloperExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new Exception("ERROR!");
+                }   
+                await next();
+            });
+            app.UseFileServer();
+        }
+    }
+}
